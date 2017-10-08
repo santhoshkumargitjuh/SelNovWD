494cd46b8631de3f3657957ffb693d5612fe954b
TestLeafPages <homework@testleaf.com>
2017-10-08 14:53:23 +0530

    Add files via upload

---
+++ src/main/java/learnTestNG/MergeLead.java
@@ -0,0 +1,41 @@
+package leaf.Lead;
+
+import org.testng.annotations.Test;
+
+import wrappers.ProjectSpecificWrapper;
+
+
+public class MergeLead extends ProjectSpecificWrapper {
+	
+	@Test//(groups="sanity", dependsOnGroups = {"smoke"})
+	public void mergeLead() throws Exception{
+		
+		clickByLink("Leads");
+		clickByLink("Merge Leads");
+		clickByXpath("(//img[@alt='Lookup'])[1]");
+		switchToLastWindow();
+		enterByName("firstName", "Gopinath");
+		clickByXpath("//button[contains(text(),'Find Leads')]");
+		Thread.sleep(2000);
+		String sText = getTextByXpath("(//div[@class='x-grid3-cell-inner x-grid3-col-partyId'])[1]/a");
+		System.out.println(sText);
+		clickByXpathNoSnap("(//div[@class='x-grid3-cell-inner x-grid3-col-partyId'])[1]/a");
+		switchToParentWindow();
+		clickByXpath("(//img[@alt='Lookup'])[2]");
+		switchToLastWindow();
+		enterByName("firstName", "Babu");
+		clickByXpath("//button[contains(text(),'Find Leads')]");
+		Thread.sleep(2000);
+		clickByXpathNoSnap("(//div[@class='x-grid3-cell-inner x-grid3-col-partyId'])[1]/a");
+		switchToParentWindow();
+		clickByLinkNoSnap("Merge");
+		acceptAlert();
+		clickByLink("Find Leads");
+		enterByXpath("//label[contains(text(),'Lead ID:')]/following::input", sText);
+		clickByXpath("//button[contains(text(),'Find Leads')]");
+		Thread.sleep(2000);
+		verifyTextContainsByXpath("//div[@class='x-paging-info']", "No records to display");
+			
+	}
+
+}
